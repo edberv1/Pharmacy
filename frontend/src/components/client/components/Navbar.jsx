504de8af905f7173ca497f852bf1c7585696c2c8
selmanvuqiterna <selmanvuqitern@gmail.com>
2024-04-08 14:24:53 +0200

    cmm

--- frontend/src/components/client/components/Navbar.jsx
+++ frontend/src/components/client/components/Navbar.jsx
@@ -4,7 +4,7 @@ function Navbar() {
       <nav className="bg-white border-gray-200 dark:bg-gray-900">
         <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4">
           <a
-            href="https://flowbite.com"
+            href="#"
             className="flex items-center space-x-3 rtl:space-x-reverse"
           >
             <img
