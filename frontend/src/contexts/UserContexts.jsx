08693b85c5556354873ef0ef84d6287ee9fe9f6b
Edber Vuciterna <vuciternaedber@gmail.com>
2024-04-12 11:15:39 +0200

    refreshing page bug fixed

--- frontend/src/contexts/UserContexts.jsx
+++ frontend/src/contexts/UserContexts.jsx
@@ -3,20 +3,16 @@ import { createContext, useState, useEffect } from "react";
 export const UserContext = createContext();
 
 const UserProvider = ({ children }) => {
-  const [user, setUser] = useState({
-    email: null,
-    role: null,
-  });
-
-  // Add this useEffect hook
-  useEffect(() => {
+  const initializeState = () => {
     const email = localStorage.getItem("email");
     const role = localStorage.getItem("role");
 
-    if (email && role) {
-      setUser({ email, role });
-    }
-  }, []);
+    return email && role ? { email, role } : { email: null, role: null };
+  };
+
+  const [user, setUser] = useState(initializeState);
+
+  // ...
 
   return (
     <UserContext.Provider value={{ user, setUser }}>
@@ -24,5 +20,6 @@ const UserProvider = ({ children }) => {
     </UserContext.Provider>
   );
 };
+
 
 export default UserProvider;
