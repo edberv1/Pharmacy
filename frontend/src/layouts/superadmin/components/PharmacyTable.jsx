7e92f3a37a3525db5d9821b962c829b48eb9329e
BesimCmega <besimcmega7@gmail.com>
2024-05-14 14:00:55 +0200

    Arranged the filter button

--- frontend/src/layouts/superadmin/components/PharmacyTable.jsx
+++ frontend/src/layouts/superadmin/components/PharmacyTable.jsx
@@ -172,19 +172,27 @@ function PharmacyTable() {
               </form>
             </div>
             <div className="w-full md:w-auto flex flex-col md:flex-row space-y-2 md:space-y-0 items-stretch md:items-center justify-end md:space-x-3 flex-shrink-0">
+              <button
+                type="button"
+                onClick={openModal}
+                className="flex items-center justify-center text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-green-600 dark:hover:bg-green-700 focus:outline-none dark:focus:ring-green-800"
+              >
+                <i className="fa-solid fa-prescription-bottle-medical pr-2"></i>
+                Create Pharmacy
+              </button>
               <div className="relative">
                 <button
                   id="filterDropdownButton"
                   onClick={toggleDropdown}
-                  className="w-full md:w-auto flex items-center justify-center py-2 px-4 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+                  className="w-full md:w-auto flex items-center justify-center py-2 px-4 text-sm font-medium text-white bg-gray-800 rounded-lg border border-gray-200 hover:bg-gray-700 focus:outline-none dark:bg-gray-600 dark:border-gray-600 dark:hover:bg-gray-700"
                   type="button"
                 >
                   Filter
                   <svg
-                    className="-mr-1 ml-1.5 w-5 h-5"
-                    fillRule="currentColor"
-                    viewBox="0 0 20 20"
+                    className="-mr-1 ml-1.5 h-5 w-5"
                     xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
                     aria-hidden="true"
                   >
                     <path
@@ -232,14 +240,6 @@ function PharmacyTable() {
                   </div>
                 </div>
               </div>
-              <button
-                type="button"
-                onClick={openModal}
-                className="flex items-center justify-center text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-green-600 dark:hover:bg-green-700 focus:outline-none dark:focus:ring-green-800"
-              >
-                <i className="fa-solid fa-prescription-bottle-medical pr-2"></i>
-                Create Pharmacy
-              </button>
               <CreatePharmacyModal isOpen={isModalOpen} onClose={closeModal} />
             </div>
           </div>
