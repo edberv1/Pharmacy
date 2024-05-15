94a00680fb7f6737a9948a6848eea2332fecd32e
drenbajraktari <drenbajraktari3@gmail.com>
2024-05-15 17:36:29 +0200

    Fixing the dropdown of pharmacyName in the create product modal

--- frontend/src/layouts/admin/components/ProductModal/CreateProductModal.jsx
+++ frontend/src/layouts/admin/components/ProductModal/CreateProductModal.jsx
@@ -11,96 +11,19 @@ function CreateProductModal({ isOpen, onClose, pharmacy }) {
     stock: "",
   });
 
-  const [pharmacyOptions, setPharmacyOptions] = useState([]);
-
-  useEffect(() => {
-    if (pharmacy) {
-      setPharmacyOptions([{ id: pharmacy.id, name: pharmacy.name }]);
-    }
-  }, [pharmacy]);
-
-  const [formSubmitted, setFormSubmitted] = useState(false); // Track form submission
-  const [error, setError] = useState(null); // State for handling errors
-  // const [users, setUsers] = useState([]);
-  const [pharmacies, setPharmacies] = useState([]);
-
   const modalRef = useRef(null);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData((prevData) => ({ ...prevData, [name]: value }));
-    setFormSubmitted(false);
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    // Here you can add your logic to create a user
-    try {
-      // Make HTTP POST request to create user with token included in headers
-      const response = await fetch(
-        "http://localhost:8081/admin/createProduct",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: "Bearer " + localStorage.getItem("token"),
-          },
-          body: JSON.stringify(formData),
-        }
-      );
-
-      // Check if the request was successful
-      if (!response.ok) {
-        // Handle non-200 response status
-        const errorMessage = await response.text();
-        throw new Error(errorMessage);
-      }
-
-      const data = await response.json();
-      // setUsers(data.users);
-      console.log(data); // Log the response if needed
-      onClose();
-      setFormSubmitted(true); // Set form submission status to true
-    } catch (error) {
-      setError(error.message); // Set error state
-      setFormSubmitted(false); // Set form submission status to false
-    }
+    // Here you can add your logic to create a product
   };
 
-  useEffect(() => {
-    const fetchPharmacies = async () => {
-      try {
-        const response = await fetch(
-          "http://localhost:8081/admin/getPharmaciesForUser", // Update API endpoint
-          {
-            method: "GET",
-            headers: {
-              "Content-Type": "application/json",
-              Authorization: "Bearer " + localStorage.getItem("token"),
-            },
-          }
-        );
-        if (!response.ok) {
-          throw new Error("Failed to fetch pharmacies");
-        }
-        const data = await response.json();
-        setPharmacies(data); // Update state with fetched pharmacies
-      } catch (error) {
-        console.error("Error fetching pharmacies: ", error);
-        // Handle error
-      }
-    };
-
-    fetchPharmacies();
-  }, []);
-
   const handleClose = () => {
-    setFormData({
-      //   roleId : "", ???
-      name: "",
-      produced: "",
-      stock: "",
-    });
     onClose();
   };
 
@@ -115,13 +38,6 @@ function CreateProductModal({ isOpen, onClose, pharmacy }) {
       document.removeEventListener("mousedown", handleOutsideClick);
     };
   }, [isOpen, onClose]);
-
-  useEffect(() => {
-    // Reload the page if form is submitted successfully
-    if (formSubmitted) {
-      window.location.reload();
-    }
-  }, [formSubmitted]);
 
   if (!isOpen) {
     return null;
@@ -179,7 +95,7 @@ function CreateProductModal({ isOpen, onClose, pharmacy }) {
             />
           </div>
 
-          <div>
+          <div className="mb-4">
             <label className="block text-sm font-medium text-gray-600">
               Produced
             </label>
@@ -226,24 +142,15 @@ function CreateProductModal({ isOpen, onClose, pharmacy }) {
               <label className="block text-sm font-medium text-gray-600">
                 Pharmacy
               </label>
-              <select
-                name="pharmacyId"
-                value={formData.pharmacyId}
-                onChange={handleChange}
+              <input
+                type="text"
+                value={pharmacy ? pharmacy.name : ""}
                 className="mt-1 p-2 w-full border rounded-md"
-                required
-              >
-                <option value="">Select Pharmacy</option>
-                {pharmacyOptions.map((pharmacy) => (
-                  <option key={pharmacy.id} value={pharmacy.id}>
-                    {pharmacy.name}
-                  </option>
-                ))}
-              </select>
+                readOnly
+              />
             </div>
           </div>
 
-          {error && <div className="text-red-500 mt-2">{error}</div>}
           <div className="flex justify-end">
             <button
               type="button"
